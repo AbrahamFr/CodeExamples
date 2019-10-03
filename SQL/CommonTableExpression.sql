de3c188127bb4b9fab6bae24b061cd4be066bf67
Abraham <abraham.french@gmail.com>
2019-10-03 09:12:45 -0500

    Alternative option to handle NULL

--- SQL/CommonTableExpression.sql
+++ SQL/CommonTableExpression.sql
@@ -10,3 +10,22 @@
                             ON sgsg.ScanGroupId = c.ScanGroupId
                         )
                         SELECT DISTINCT ScanGroupId FROM CTE";
+                        
+                        
+                        
+ -- Alternative
+			    WITH cteCheckPointGroupHierarchy AS
+				(
+					SELECT cpg.CheckpointGroupId, CAST(NULL AS nvarchar(32)) AS ParentId
+					FROM CheckpointGroups cpg
+					WHERE cpg.CheckpointGroupId = @CheckpointGroupID
+						AND cpg.CheckpointGroupId NOT IN (Select SubGroupId FROM CheckpointGroupSubGroups)
+					UNION ALL
+					SELECT SubgroupId, cpgsg.CheckpointGroupId AS ParentId
+					FROM CheckpointGroupSubGroups cpgsg
+					INNER JOIN cteCheckPointGroupHierarchy c
+						ON cpgsg.CheckpointGroupId = c.CheckpointGroupId
+				)
+				SELECT cpgcp.* FROM CheckpointGroupCheckpoints cpgcp
+				INNER JOIN cteCheckPointGroupHierarchy cpgh
+				  ON cpgh.CheckpointGroupId = cpgcp.CheckpointGroupId";
