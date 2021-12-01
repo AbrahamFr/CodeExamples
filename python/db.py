8d0a04a98bb59d10c7d1fe97a3f57facb33487df
Abraham <abraham.french@appgate.com>
2021-12-01 12:47:53 -0600

    Async db connection in Python

---
+++ python/db.py
@@ -0,0 +1,12 @@
+import asyncio
+
+from app.db.init_db import init_db
+from app.db.session import SessionLocal
+
+async def init() -> None:
+    async with SessionLocal() as db:
+        init_db(db)
+        
+if __name__ == "__main__":
+    main()
+    asyncio.run(main())
